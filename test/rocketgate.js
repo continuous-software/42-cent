1cb9c4f0b82f4fb1eb0dbf48b5be441eb1e2d39e
lorenzofox3 <laurent34azerty@gmail.com>
2014-09-16 15:55:44 +0700

    basic prospect info

--- test/rocketgate.js
+++ test/rocketgate.js
@@ -28,32 +28,40 @@ describe('rocket gate service', function () {
                 cvv: '999'
             };
 
+            var prospect = {
+                customerFirstName: 'bob',
+                customerLastName: 'eponge',
+                customerEmail: 'bob.eponge@gmail.com'
+            };
+
             var order = {amount: '3.99'};
 
-            service.submitTransaction(order, cc, {}, {}).then(function (result) {
+            service.submitTransaction(order, cc, prospect).then(function (result) {
                 assert.equal(result.transactionId, result._original.Get(rocketGate.GatewayResponse.TRANSACT_ID), 'it should have the appropriate transactionId');
                 assert.equal(result.authCode, result._original.Get(rocketGate.GatewayResponse.AUTH_NO), 'it should have the appropriate authCode');
                 done();
             });
         });
-    });
 
-    it('should reject the promise when we get a web service error', function (done) {
-        var cc = {
-            expirationYear: '2010',
-            expirationMonth: '02',
-            cvv: '999'
-        };
+        it('should reject the promise when we get a web service error', function (done) {
+            var cc = {
+                expirationYear: '2010',
+                expirationMonth: '02',
+                cvv: '999'
+            };
 
-        var order = {amount: '3.99'};
+            var order = {amount: '3.99'};
 
-        service.submitTransaction(order, cc, {}, {}).then(function (result) {
-            throw new Error('should not get here');
-        }, function (reason) {
-            assert(reason instanceof errors.GatewayError);
-            assert.equal(reason.message, 'see response code 403 in rocket gate documentation', 'it should have the appropriate message');
-            assert(reason._original, '_original should be defined');
-            done();
+            service.submitTransaction(order, cc, {}, {}).then(function (result) {
+                throw new Error('should not get here');
+            }, function (reason) {
+                assert(reason instanceof errors.GatewayError);
+                assert.equal(reason.message, 'see response code 403 in rocket gate documentation', 'it should have the appropriate message');
+                assert(reason._original, '_original should be defined');
+                done();
+            });
         });
+
     });
+
 });
