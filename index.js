2f182dee8b1095638deff81c6c02427327c22946
lorenzofox3 <laurent34azerty@gmail.com>
2014-10-28 14:54:09 +0700

    add support for nmi payment gateway

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var BaseGateway = require('42-cent-base').BaseGateway;
 var PayFlow = require('42-cent-payflow');
 var RocketGate = require('42-cent-rocketgate');
 var VirtualMerchant = require('42-cent-virtualmerchant');
+var nmi = require('42-cent-nmi');
 var supportedGateway = {
     "Authorize.Net": AuthorizeNet,
     "PayFlow": PayFlow,
     "RocketGate": RocketGate,
-    "VirtualMerchant": VirtualMerchant
+    "VirtualMerchant": VirtualMerchant,
+    "NMI": nmi
 };
 
 /**
